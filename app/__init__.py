1ce1f5cdb6c92ff4d71081261955fd20ae118d84
Duncan Law-Green <duncan.lawgreen@gmail.com>
2013-07-08 08:26:55 +0100

    Remove db session on application shutdown

--- app/__init__.py
+++ app/__init__.py
@@ -2,3 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 from app import views, database
+
+# Remove database session when application shuts down
+
+@app.teardown_appcontext
+def shutdown_session(exception=None):
+    db_session.remove()
