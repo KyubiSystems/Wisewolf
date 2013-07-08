54e7a5c23424fe2a3c5a63a21e53a69ef73a1b57
Duncan Law-Green <duncan.lawgreen@gmail.com>
2013-07-09 00:02:41 +0100

    Add error page handling

--- app/views.py
+++ app/views.py
@@ -29,3 +29,14 @@ def opml_import():
 @app.route('/gallery')
 def gallery():
     return render_template("gallery.html")
+
+# Error handling
+
+@app.errorhandler(404)
+def internal_error(error):
+    return render_template("404.html"), 404
+
+@app.errorhandler(500)
+def internal_error(error):
+    db.session.rollback()
+    return render_template("500.html"), 500
