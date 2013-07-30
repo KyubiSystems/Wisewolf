a4b65e2de83e920cd41f469db636f348ab2a1a42
Duncan Law-Green <duncan.lawgreen@gmail.com>
2013-07-30 23:26:55 +0100

    Working on lightweight feed finding

--- app/feedfind.py
+++ app/feedfind.py
@@ -6,13 +6,32 @@ Usage:
 
 import urllib, urlparse, re, sys, HTMLParser
 
+# Declare valid RSS and ATOM feed types for autodetection
+
+FEED_TYPES = ('application/rss+xml',
+              'text/xml',
+              'application/atom+xml',
+              'application/x.atom+xml',
+              'application/x-atom+xml')
+
+# Define feed finder class
+
 class FeedFind(HTMLParser.HTMLParser):
     def __init__(self):
         HTMLParser.HTMLParser.__init__(self)
+        self.data = [] # list of returned feed URLs
+
+# Search for <LINK> tag in header
 
     def handle_starttag(self, tag, attributes):
-        if tag != 'a' : return
+        if tag != 'link' : return
+        for name, value in attributes:
+            if name == 'type' && value not in FEED_TYPES : return
+            if name == 'href' && isFeedLink(value): 
+                fulluri = makeFullURI(value)
+                self.data.append(fulluri)
 
+# Convert partial to full URIs
 
 def makeFullURI(uri):
     uri = uri.strip()
@@ -22,6 +41,8 @@ def makeFullURI(uri):
         if uri.startswith('%s://' % x):
             return uri
     return 'http://%s' % uri
+
+# Check for feed link extension
 
 def isFeedLink(link):
     return link[-4:].lower() in ('.rss', '.rdf', '.xml', '.atom')
