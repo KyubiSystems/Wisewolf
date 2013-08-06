6994cb743cd38fc0092b528ec872e3b6c15d079c
Duncan Law-Green <duncan.lawgreen@gmail.com>
2013-08-06 23:57:45 +0100

    OPML parsing

--- app/Opml.py
+++ app/Opml.py
@@ -16,6 +16,33 @@ tree = ET.parse(filename)
 
 root = tree.getroot()
 
+# Use XPath to parse header items
+title = root.find("./head/title")
+print title.text
+
+dateCreated = root.find("./head/dateCreated")
+print dateCreated.text
+
+dateModified = root.find("./head/dateModified")
+print dateModified.text
+
+ownerName = root.find("./head/ownerName")
+print ownerName.text
+
+ownerEmail = root.find("./head/ownerEmail")
+print ownerEmail.text
+
+opmlLink = root.find("./head/link")
+print opmlLink.text
+
+print '-------------'
+
+# Parse body items
 for child in root.iter('outline'):
-    print child.tag, child.attrib
+    if 'url' not in child.attrib.keys():
+        print "\nCategory: "+child.attrib['text']
+    else:
+        print "\nLink:\n-----"
+        for (key, value) in child.attrib.iteritems():
+            print key+": "+value
 
