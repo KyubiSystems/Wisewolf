71a11665a5a1e957be7e917a0491d60fe90c6698
Duncan Law-Green <duncan.lawgreen@gmail.com>
2014-02-05 23:25:37 +0000

    Starting defaults loader

--- app/db_utils.py
+++ app/db_utils.py
@@ -22,7 +22,30 @@ def create_db():
 
 
 # load default feeds in DB
-# Defaults should be in OPML format?
+# Start with defaults file in PSV
+# Consider moving to OPML later?
 def load_defaults():
-    print "In defaults loader...",
+    print "Starting defaults loader...",
+
+    # Open defaults file
+    with open(DEFAULTS_FILE, 'r') as f:
+        rows = f.readlines()
+    f.close()
+
+    # Define database
+    db = SqliteDatabase(DB_FILE)
+
+    # Connect to database
+    db.connect()
+
+    # Iterate over default feeds list
+    # PSV format name|url|
+    # Should scan and update category table first
+    for row in rows:
+        (name, url, category) = row.split('|')
+        # Update Feeds table
+        Feed.create(name=name, url=url, category=category)
+
+    print "done"
+
 
