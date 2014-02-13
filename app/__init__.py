c3a4e0a565b75418f9db246b69584e296283c419
Duncan Law-Green <duncan.lawgreen@gmail.com>
2014-02-13 21:03:23 +0000

    Flask route testing

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,30 @@
 from flask import Flask
+from flask import g
+from flask import request
+from flask import session
+from flask import url_for, render_template, abort
+
+# Import configuration
+from config import *
+from models import *
 
 app = Flask(__name__)
-from app import views, database
 
-# Remove database session when application shuts down
+database = SqliteDatabase(DB_FILE)
 
-@app.teardown_appcontext
-def shutdown_session(exception=None):
-    db_session.remove()
+# Request handlers provided by Flask
+# Used to create and tear down a database connection
+# on each request
+
+@app.before_request
+def before_request():
+    g.db = database
+    g.db.connect()
+
+@app.after_request
+def after_request(response):
+    g.db.close()
+    return response
+
+# Import views
+from views import *
