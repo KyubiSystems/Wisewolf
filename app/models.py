e6ca1b551c5413406303ce97271c217f907a4f7a
Duncan Law-Green <duncan.lawgreen@gmail.com>
2014-08-11 23:19:29 +0100

    enhanced models, delete SocketIO dependence for now

--- app/models.py
+++ app/models.py
@@ -6,7 +6,7 @@ Wisewolf RSS Reader
 
 from config import *
 from peewee import *
-import datetime
+from datetime import datetime
 
 # define database
 db = SqliteDatabase(DB_FILE, threadlocals=True)
@@ -22,14 +22,28 @@ class BaseModel(Model):
 class Category(BaseModel):
     name = CharField(unique=True)
     comment = TextField()
+    order = IntegerField() # display order index of categories
+# Peewee timestamps
+    created_at = DateTimeField(default=datetime.now())
+    updated_at = DateTimeField(null=True)
+    deleted_at = DateTimeField(null=True)
 
+
+    class Meta:
+        order_by = ('order',)
+
+    def __unicode__(self):
+        return self.name
+
+    def __str__(self):
+        return self.__unicode__()
 
 class Feed(BaseModel):
     name = CharField()
     url = CharField(max_length=512)
     category = ForeignKeyField(Category, related_name='feeds')
     version = CharField(max_length=10)
-    last_updated = DateTimeField(default=datetime.datetime.now())
+    last_updated = DateTimeField(default=datetime.now())
     last_modified = CharField(max_length=64, default='')
     etag = CharField(max_length=64, default='')
     comment = TextField()
@@ -39,7 +53,19 @@ class Feed(BaseModel):
     errors = IntegerField(default=0)  # Number of errors seen in feed loading
     inactive = BooleanField(default=False)
     favicon = CharField(max_length=64)
+# Peewee timestamps
+    created_at = DateTimeField(default=datetime.now())
+    updated_at = DateTimeField(null=True)
+    deleted_at = DateTimeField(null=True)
 
+    class Meta:
+        order_by = ('-last_updated',)
+
+    def __unicode__(self):
+        return self.name
+
+    def __str__(self):
+        return self.__unicode__()
 
 class Post(BaseModel):
     title = CharField()
@@ -47,14 +73,39 @@ class Post(BaseModel):
     description = TextField()
     content = TextField()
     feed = ForeignKeyField(Feed, on_delete='cascade', related_name='posts')
-    published = DateTimeField(default=datetime.datetime.now())
+    published = DateTimeField(default=datetime.now())
     is_read = BooleanField(default=False)
     is_favourite = BooleanField(default=False)
+# Peewee timestamps
+    created_at = DateTimeField(default=datetime.now())
+    updated_at = DateTimeField(null=True)
+    deleted_at = DateTimeField(null=True)
 
+    class Meta:
+        order_by = ('-published',)
+
+    def __unicode__(self):
+        return self.title
+
+    def __str__(self):
+        return self.__unicode__()
 
 class Image(BaseModel):
     feed = ForeignKeyField(Feed, related_name='images')
     post = ForeignKeyField(Post, related_name='images')
     path = CharField()
     thumb = CharField(max_length=512)
-    date_created = DateTimeField(default=datetime.datetime.now())
+# Peewee timestamps
+    created_at = DateTimeField(default=datetime.now())
+    updated_at = DateTimeField(null=True)
+    deleted_at = DateTimeField(null=True)
+
+    class Meta:
+        order_by = ('-created_at',)
+
+    def __unicode__(self):
+        return self.thumb
+
+    def __str__(self):
+        return self.__unicode__()
+
