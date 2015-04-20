ccd44af575158f52e401c91ba696f58f365e4faa
Duncan Law-Green <duncan.lawgreen@gmail.com>
2015-04-20 23:01:27 +0100

    Testing save of OPML read

--- app/opml_test.py
+++ app/opml_test.py
@@ -4,6 +4,7 @@ Testing OPML import
 (c) 2015 KyubiSystems: www.kyubi.co.uk
 """
 from opml import Opml
+from models import *
 
 o = Opml.OpmlReader('./opml/testing.xml')
 #o = Opml.OpmlReader('./opml/opml-example.xml')
@@ -14,3 +15,14 @@ o.parseOpml()
 print o.version
 print o.categories
 print o.feeds
+
+# Save categories to DB
+for c in o.categories:
+    cat = Category.create(name = c.name)
+    cat.save()
+
+# Save feeds to DB
+for f in o.feeds:
+    # Check OPML version here?
+    feed = Feed.create(name = f.name, category = f.category, version = f.type, comment = f.text, url = f.xmlUrl)
+    feed.save()
