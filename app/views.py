f0a7c540fdcadce9103a12aa66c3cf762d18df42
Duncan Law-Green <duncan.lawgreen@gmail.com>
2015-05-13 00:20:33 +0100

    Added new routes

--- app/views.py
+++ app/views.py
@@ -34,8 +34,10 @@ def index():
     # Render main page template
     return render_template("index.html", categories=categories, feeds=feeds, posts=posts)
 
+# Feed routes ----------------
+
 @app.route('/feed')
-@app.route('/feed/<id>')
+@app.route('/feed/<int:id>')
 def feed(id=None):
     # Get feed number <id>
     feed = Feed.get(Feed.id == id)
@@ -46,8 +48,30 @@ def feed(id=None):
     # Render feed page template
     return render_template("feed.html", feed=feed, posts=posts)
 
+@app.route('/feed/all/refresh')
+@app.route('/feed/<int:id>/refresh')
+def refresh(id=None):
+    # Manual update of one or all feeds
+    
+    return True
+
+@app.route('/feed/all/markread')
+@app.route('/feed/<int:id>/markread')
+def markread(id=None):
+    # Manual markread of one or all feeds
+
+    return True
+
+@app.route('/feed/<int:id>/delete')
+def delete(id=None):
+    # Manual deletion of feed from database
+
+    return True
+
+# Category routes ------------
+
 @app.route('/category')
-@app.route('/category/<id>')
+@app.route('/category/<int:id>')
 def category(id=None):
     # Get category number <id>
     categories = Category.get(Category.id == id)
@@ -61,39 +85,42 @@ def category(id=None):
     # Render category page template
     return render_template("category.html", categories=categories, feeds=feeds, posts=posts)
 
-# Websocket testing
+# Gallery routes -------------
 
-@app.route('/wstest')
-def wstest():
-    return render_template("wstest.html")
+@app.route('/gallery')
+@app.route('/gallery/<int:id>')
+def gallery(id=None):
+    # Get gallery images associated with feed #id
+    return render_template("gallery.html")
 
-@app.route('/wstest2')
-def wstest2():
-    return render_template("wstest2.html")
-
-@app.route('/manage')
-def manage():
-    return render_template("manage.html")
+# Management routes ----------
 
 @app.route('/settings')
 def settings():
     return render_template("settings.html")
 
+# Import OPML
 @app.route('/import')
 def opml_import():
     return render_template("import.html")
 
-@app.route('/gallery')
-def gallery():
-    return render_template("gallery.html")
+# Websocket testing ----------
 
-# Error handling
+@app.route('/test/websocket')
+def wstest():
+    return render_template("ws_echo_client.html")
+
+@app.route('/test/websocket2')
+def wstest2():
+    return render_template("ws_jsontime_client.html")
+
+# Error handling -------------
 
 @app.errorhandler(404)
 def internal_error(error):
-    return render_template("404.html"), 404
+    return render_template("404.html", error=error), 404
 
 @app.errorhandler(500)
 def internal_error(error):
     db.session.rollback()
-    return render_template("500.html"), 500
+    return render_template("500.html", error=error), 500
