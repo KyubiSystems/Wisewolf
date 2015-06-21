edd403dcb3e29c3eda6654149bd78b02a77ab223
Duncan Law-Green <duncan.lawgreen@gmail.com>
2015-06-21 21:32:00 +0100

    Modified startup;

---
+++ start.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import os
+import subprocess
+
+# Get script directory
+
+topdir = os.path.dirname(os.path.realpath(__file__))
+appdir = topdir + '/app'
+
+# Start Wisewolf RSS server backend
+
+print "Wisewolf RSS server v0.04 starting... ",
+subprocess.Popen(["./server.py"], cwd=appdir)
+print 'OK'
+
+# Start Wisewolf web frontend
+
+print "Wisewolf frontend v0.04 starting... ",
+subprocess.Popen(["./frontend.py"], cwd=topdir)
+print 'OK'
